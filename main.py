89c0a482d796ad3d4ed9b11c4066b1e2d5dc438e
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2022-01-08 21:32:13 +0100

    measure every 3 minutes

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import adafruit_dht
 import requests
 import datetime
 from typing import List
+import asyncio
 
+loop = asyncio.get_event_loop()
 dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
 
 #URL = "http://localhost:3030"
-URL = "http://192.168.0.110:8080"
-address = "{}/add_temp".format(URL)
+URL = "http://192.168.0.100:8080"
+hour_address = "{}/add_temp".format(URL)
+last_address = "{}/add_last_temp".format(URL)
 
 temps: List[float] = []
 
@@ -18,7 +21,7 @@ def meassure():
         temperature_c = dhtDevice.temperature
         temps.append(float(temperature_c)) #type: ignore
     except RuntimeError as error:
-        time.sleep(2.0)
+        time.sleep(2)
         meassure()
     except Exception as error:
         dhtDevice.exit()
@@ -29,7 +32,7 @@ def repeat_meassure():
         meassure()
         time.sleep(2)
 
-def add_temp():
+async def add_temp(address: str):
     repeat_meassure()
     temp = sum(temps) / len(temps)
     date = datetime.datetime.now()
@@ -46,6 +49,20 @@ def add_temp():
     except requests.ConnectionError as error:
         print(error)
 
-while True:
-    add_temp()
-    time.sleep(60 * 60)
+async def wait_till_whole():
+    date = datetime.datetime.now()
+    await asyncio.sleep((59 - date.minute) * 60)
+
+async def every_hour():
+    await add_temp(hour_address)
+    await wait_till_whole()
+
+async def every_minutes():
+    # little bit over 3 minutes
+    await asyncio.sleep(190)
+    await add_temp(last_address)
+
+loop.create_task(every_hour())
+loop.create_task(every_minutes())
+
+loop.run_forever()
