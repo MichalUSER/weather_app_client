d269223bfc2c52e85941fcf581aeaa816b00ba6e
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2022-02-04 08:40:20 +0100

    update

--- async.py
+++ async.py
@@ -4,7 +4,9 @@ import adafruit_dht
 import requests
 import datetime
 from typing import List
+import asyncio
 
+loop = asyncio.get_event_loop()
 dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
 
 #URL = "http://localhost:3030"
@@ -14,24 +16,24 @@ last_address = "{}/add_last_temp".format(URL)
 
 temps: List[float] = []
 
-def meassure():
+async def meassure():
     try:
         temperature_c = dhtDevice.temperature
         temps.append(float(temperature_c)) #type: ignore
     except RuntimeError as error:
         time.sleep(2)
-        meassure()
+        await meassure()
     except Exception as error:
         dhtDevice.exit()
         raise error
 
-def repeat_meassure():
+async def repeat_meassure():
     for _ in range(5):
-        meassure()
+        await meassure()
         time.sleep(2)
 
-def add_temp(address: str):
-    repeat_meassure()
+async def add_temp(address: str):
+    await repeat_meassure()
     temp = sum(temps) / len(temps)
     date = datetime.datetime.now()
     data = {
@@ -47,18 +49,23 @@ def add_temp(address: str):
     except requests.ConnectionError as error:
         print(error)
 
-def wait_till_whole():
+async def wait_till_whole():
     date = datetime.datetime.now()
     #print("measured at {}".format(date.hour))
-    time.sleep((59 - date.minute) * 60)
+    await asyncio.sleep((59 - date.minute) * 60)
 
-add_temp(last_address)
-time.sleep(60)
+async def every_hour():
+    while True:
+        await add_temp(hour_address)
+        await wait_till_whole()
 
-while True:
-    date = datetime.datetime.now()
-    if date.minute == 0:
-        add_temp(hour_address)
-    elif date.minute % 4.0 == 0.0:
-        add_temp(last_address)
-    time.sleep(50)
+async def every_minutes():
+    while True:
+        # little bit over 3 minutes
+        await asyncio.sleep(270)
+        await add_temp(last_address)
+
+loop.create_task(every_hour())
+loop.create_task(every_minutes())
+
+loop.run_forever()
